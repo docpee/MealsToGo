a2185eb95150e98bcb98504782096ad8711f10e7
docpee <alessandro.prampolini@gmail.com>
2021-04-26 14:08:00 +0200

    Implementata Flatlist

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,22 +1,54 @@
 import React from "react";
-import { StyleSheet, View, SafeAreaView, StatusBar } from "react-native";
+import { SafeAreaView, StatusBar, FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
+import styled from "styled-components";
 
-import { RestaurantInfo } from "../components/restaurants-info.components";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.components";
+
+import { Spacer } from "../../../components/spacer/spacer.component";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  /* Se esiste StatusBar.currentHeight fai margin-top: ${StatusBar.currentHeight}px; altrimenti no (Short Circuit Expression)*/
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px;`}
+`;
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
 
 export const RestaurantsScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <View style={styles.search}>
+  <SafeArea>
+    <SearchContainer>
       <Searchbar />
-    </View>
-    <View style={styles.list}>
-      <RestaurantInfo />
-    </View>
-  </SafeAreaView>
+    </SearchContainer>
+    <FlatList
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+        { name: 7 },
+        { name: 8 },
+        { name: 9 },
+        { name: 10 },
+        { name: 11 },
+        { name: 12 },
+        { name: 13 },
+        { name: 14 },
+        { name: 15 },
+      ]}
+      renderItem={() => (
+        <>
+          <Spacer position="bottom" size="large">
+            <RestaurantInfoCard />
+          </Spacer>
+        </>
+      )}
+      keyExtractor={(item) => item.name}
+      contentContainerStyle={{ padding: 16 }}
+    />
+  </SafeArea>
 );
-
-const styles = StyleSheet.create({
-  container: { flex: 1, marginTop: StatusBar.currentHeight },
-  search: { padding: 16 },
-  list: { flex: 1, padding: 16, backgroundColor: "blue" },
-});
