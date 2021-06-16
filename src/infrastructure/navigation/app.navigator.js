cbfbbb852abf018d789b301c6f7b7faec3370b4a
docpee <alessandro.prampolini@gmail.com>
2021-06-16 12:16:19 +0200

    Aggiunta la Mappa

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -9,6 +9,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeArea } from '../../components/utility/safe-area.component';
 
 import { RestaurantsNavigator } from './restaurants.navigator';
+import {MapScreen} from "../../features/map/screens/map.screen"
 
 const TAB_ICON = {
   Restaurants: 'md-restaurant',
@@ -16,11 +17,11 @@ const TAB_ICON = {
   Map: 'md-map'
 };
 
-const Map = () => (
-  <SafeArea>
-    <Text>Map</Text>
-  </SafeArea>
-);
+// const Map = () => (
+//   <SafeArea>
+//     <Text>Map</Text>
+//   </SafeArea>
+// );
 
 const Settings = () => (
   <SafeArea>
@@ -49,7 +50,7 @@ export const AppNavigator = () => (
       }}
     >
       <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-      <Tab.Screen name="Map" component={Map} />
+      <Tab.Screen name="Map" component={MapScreen} />
       <Tab.Screen name="Settings" component={Settings} />
     </Tab.Navigator>
   </NavigationContainer>
